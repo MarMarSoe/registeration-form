8f872c4429c2d70edbb7b4a87924f017b972a7b8
Mar Mar Soe <marmarsoe1995@gmail.com>
2024-02-10 23:45:55 +0900

    fix: add custom onFocus color

--- src/components/AgeInput/AgeInput.jsx
+++ src/components/AgeInput/AgeInput.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./AgeInput.css";
 
 const AgeInput = ({ label, name, register, error }) => {
+
+  const [isFocus, setIsFocus] = useState(false);
+
   return (
     <div className="age-input age-input--margin-top">
       <label className="age-input__label age-input__label--margin-bottom">
@@ -15,8 +19,10 @@ const AgeInput = ({ label, name, register, error }) => {
             message: "半角数字以外は使用できません",
           },
         })}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
         className={`age-input__box ${
-          error ? "age-input__box-color-error" : ""
+          error ? "age-input__box-color-error" : isFocus ? "age-input__box-color-focus" : ""
         } `}
         maxLength="2"
       />
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./Input.css";
 
 const Input = ({ label, name, register, error }) => {
+  const [isFocus, setIsFocus] = useState(false);
+
   return (
     <div className="form-input">
       <label className="form-input__label">
@@ -15,8 +18,14 @@ const Input = ({ label, name, register, error }) => {
             message: "記号・数字は使用できません",
           },
         })}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
         className={`form-input__box ${
-          error ? "form-input__box-color-error" : ""
+          error
+            ? "form-input__box-color-error"
+            : isFocus
+            ? "form-input__box-color-focus"
+            : ""
         }`}
       />
       {error && <p className="name-error-msg">{error}</p>}

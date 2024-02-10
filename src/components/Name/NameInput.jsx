054fa378dca1eef9be8f14c8c19b7cd060c03055
Mar Mar Soe <marmarsoe1995@gmail.com>
2024-02-10 15:15:48 +0900

    refactor: error message handling

--- src/components/Name/NameInput.jsx
+++ src/components/Name/NameInput.jsx
@@ -2,37 +2,35 @@ import { useState } from "react";
 import Input from "../Input";
 import Title from "../Title";
 
-const NameInput = ({ register, errors, isValid, isDirty }) => {
+const NameInput = ({ register, errors }) => {
   return (
     <>
       <Title title="名前" className="register__subtitle--margin-top" />
-      <Input register={register} label="姓" errors={errors} name="firstname" />
-      {errors.firstname && (
-        <p className="error-msg">{errors.firstname.message}</p>
-      )}
-      <Input register={register} label="名" errors={errors} name="lastname" />
-      {errors.lastname && (
-        <p className="error-msg">{errors.lastname.message}</p>
-      )}
+      <Input
+        register={register}
+        label="姓"
+        error={errors.firstname && errors.firstname.message}
+        name="firstname"
+      />
+      <Input
+        register={register}
+        label="名"
+        errors={errors}
+        name="lastname"
+        error={errors.lastname && errors.lastname.message}
+      />
       <Input
         register={register}
         label="氏（カタカナ）"
-        errors={errors}
+        error={errors.firstnameKana && errors.firstnameKana.message}
         name="firstnameKana"
       />
-      {errors.firstnameKana && (
-        <p className="error-msg">{errors.firstnameKana.message}</p>
-      )}
-
       <Input
         register={register}
         label="名（カタカナ）"
-        errors={errors}
+        error={errors.lastnameKana && errors.lastnameKana.message}
         name="lastnameKana"
       />
-      {errors.lastnameKana && (
-        <p className="error-msg">{errors.lastnameKana.message}</p>
-      )}
     </>
   );
 };
